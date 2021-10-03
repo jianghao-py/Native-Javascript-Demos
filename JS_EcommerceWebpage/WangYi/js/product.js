25c7623d0b355fcc91f6af84b66c36f82105dd0d
Jianghao <ljh1471647441@gmail.com>
2021-10-03 22:36:04 +0800

    up

--- JS_EcommerceWebpage/WangYi/js/product.js
+++ JS_EcommerceWebpage/WangYi/js/product.js
@@ -50,6 +50,86 @@
             phoneColor[i].className = 'active';
         }
     }
+
+    let add = document.querySelector('.number .noClick .t');
+    let minus = document.querySelector('.number .noClick .o');
+    let number = document.getElementById('in');
+    
+    let val = 1;
+    number.setAttribute("value",val);
+    console.log(number);
+
+    add.onclick = function(){
+        number.value++;
+    }
+
+    minus.onclick = function(){
+        if(number.value >0){
+            number.value--;
+        }
+        
+    }
+
+    //详情与评价选项卡
+	let as=yx.ga('#bottom .title a');
+	let tabs=yx.ga('#bottom .content>div');
+	let ln=0;
+	
+	for(let i=0;i<as.length;i++){
+		as[i].onclick=function(){
+			as[ln].className='';
+			tabs[ln].style.display='none';
+			
+			this.className='active';
+			tabs[i].style.display='block';
+			
+			ln=i;
+		};
+	}
+
+    let comment = yx.ga('.evaluate .border .eTitle>div');
+    let currentIndex = 0;
+    for(let i=0;i<comment.length;i++){
+        comment[i].onclick = function(){
+            comment[currentIndex].className = '';
+            
+            comment[i].className = 'active';
+            currentIndex = i;
+        }
+    }
+
+    let smallPic = yx.ga('.smallImg span');
+    
+    for(let i=0;i<smallPic.length;i++){
+        smallPic[i].onclick = function(){
+            let bigPic = this.parentNode.parentNode.lastElementChild;
+            bigPic.style.opacity = 1;
+            bigPic.style.height = '510px';
+            
+            let prev = bigPic.querySelector('.prev');
+            let next = bigPic.querySelector('.next');
+            picArry = bigPic.querySelectorAll('.carouselImgCon ul li')
+            
+            prev.onclick = function(){
+                picArry[0].style.display = 'block';
+            }
+
+            next.onclick = function(){
+                picArry[0].style.display = 'none';
+                
+                
+            }
+            
+            let close = bigPic.querySelector('.close');
+            close.onclick = function(){
+                bigPic.style.opacity = 0;
+                bigPic.style.height = '0';
+            }
+        }
+    }
+    
+
+
     
 })();
 
